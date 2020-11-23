b45ee264d410d4754282204488263a7ba9f5d3ff
rupakdey <rupakdey1998@gmail.com>
2020-11-23 12:01:05 +0530

    Added Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+// import { ElementsModule } from './elements/elements.module';
+import { AppRoutingModule } from './app-routing.module';
+// import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    // ElementsModule,
+    // CollectionsModule,
+    SharedModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
